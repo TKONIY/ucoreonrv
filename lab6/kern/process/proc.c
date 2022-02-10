b0f7ca010b4b4ef3d3310e3ac3ea82268c526fe3
Yangshen⚡Deng <yangshen.d@bupt.edu.cn>
2022-02-11 01:13:14 +0800

    finish lab6

--- lab6/kern/process/proc.c
+++ lab6/kern/process/proc.c
@@ -126,6 +126,7 @@ alloc_proc(void) {
         proc->lab6_run_pool.left = proc->lab6_run_pool.right = proc->lab6_run_pool.parent = NULL;
         proc->lab6_stride = 0;
         proc->lab6_priority = 0;
+        proc->time_slice = 0; // LAB 6
     }
     return proc;
 }
--- lab6/kern/schedule/default_sched_stride.c
+++ lab6/kern/schedule/default_sched_stride.c
@@ -14,14 +14,13 @@
 /* The compare function for two skew_heap_node_t's and the
  * corresponding procs*/
 static int
-proc_stride_comp_f(void *a, void *b)
-{
-     struct proc_struct *p = le2proc(a, lab6_run_pool);
-     struct proc_struct *q = le2proc(b, lab6_run_pool);
-     int32_t c = p->lab6_stride - q->lab6_stride;
-     if (c > 0) return 1;
-     else if (c == 0) return 0;
-     else return -1;
+proc_stride_comp_f(void *a, void *b) {
+    struct proc_struct *p = le2proc(a, lab6_run_pool);
+    struct proc_struct *q = le2proc(b, lab6_run_pool);
+    int32_t c = p->lab6_stride - q->lab6_stride;
+    if (c > 0) return 1;
+    else if (c == 0) return 0;
+    else return -1;
 }
 
 /*
@@ -37,11 +36,14 @@ proc_stride_comp_f(void *a, void *b)
  */
 static void
 stride_init(struct run_queue *rq) {
-     /* LAB6: YOUR CODE 
-      * (1) init the ready process list: rq->run_list
-      * (2) init the run pool: rq->lab6_run_pool
-      * (3) set number of process: rq->proc_num to 0       
-      */
+    /* LAB6: YOUR CODE
+     * (1) init the ready process list: rq->run_list
+     * (2) init the run pool: rq->lab6_run_pool
+     * (3) set number of process: rq->proc_num to 0
+     */
+    list_init(&(rq->run_list));
+    rq->lab6_run_pool = NULL; // TODO: or init ?
+    rq->proc_num = 0;
 }
 
 /*
@@ -53,21 +55,28 @@ stride_init(struct run_queue *rq) {
  *
  * proc->time_slice denotes the time slices allocation for the
  * process, which should set to rq->max_time_slice.
- * 
+ *
  * hint: see libs/skew_heap.h for routines of the priority
  * queue structures.
  */
 static void
 stride_enqueue(struct run_queue *rq, struct proc_struct *proc) {
-     /* LAB6: YOUR CODE 
-      * (1) insert the proc into rq correctly
-      * NOTICE: you can use skew_heap or list. Important functions
-      *         skew_heap_insert: insert a entry into skew_heap
-      *         list_add_before: insert  a entry into the last of list   
-      * (2) recalculate proc->time_slice
-      * (3) set proc->rq pointer to rq
-      * (4) increase rq->proc_num
-      */
+    /* LAB6: YOUR CODE
+     * (1) insert the proc into rq correctly
+     * NOTICE: you can use skew_heap or list. Important functions
+     *         skew_heap_insert: insert a entry into skew_heap
+     *         list_add_before: insert  a entry into the last of list
+     * (2) recalculate proc->time_slice
+     * (3) set proc->rq pointer to rq
+     * (4) increase rq->proc_num
+     */
+//    assert(list_empty(&(proc->run_link)));
+    rq->lab6_run_pool = skew_heap_insert(rq->lab6_run_pool, &proc->lab6_run_pool, proc_stride_comp_f);
+    if (proc->time_slice == 0 || proc->time_slice > rq->max_time_slice) {
+        proc->time_slice = rq->max_time_slice;
+    }
+    proc->rq = rq;
+    rq->proc_num++;
 }
 
 /*
@@ -80,13 +89,16 @@ stride_enqueue(struct run_queue *rq, struct proc_struct *proc) {
  */
 static void
 stride_dequeue(struct run_queue *rq, struct proc_struct *proc) {
-     /* LAB6: YOUR CODE 
-      * (1) remove the proc from rq correctly
-      * NOTICE: you can use skew_heap or list. Important functions
-      *         skew_heap_remove: remove a entry from skew_heap
-      *         list_del_init: remove a entry from the  list
-      */
+    /* LAB6: YOUR CODEz87
+     * (1) remove the proc from rq correctly
+     * NOTICE: you can use skew_heap or list. Important functions
+     *         skew_heap_remove: remove a entry from skew_heap
+     *         list_del_init: remove a entry from the  list
+     */
+    rq->lab6_run_pool = skew_heap_remove(rq->lab6_run_pool, &proc->lab6_run_pool, proc_stride_comp_f);
+    rq->proc_num--;
 }
+
 /*
  * stride_pick_next pick the element from the ``run-queue'', with the
  * minimum value of stride, and returns the corresponding process
@@ -102,13 +114,18 @@ stride_dequeue(struct run_queue *rq, struct proc_struct *proc) {
  */
 static struct proc_struct *
 stride_pick_next(struct run_queue *rq) {
-     /* LAB6: YOUR CODE 
-      * (1) get a  proc_struct pointer p  with the minimum value of stride
-             (1.1) If using skew_heap, we can use le2proc get the p from rq->lab6_run_pol
-             (1.2) If using list, we have to search list to find the p with minimum stride value
-      * (2) update p;s stride value: p->lab6_stride
-      * (3) return p
-      */
+    /* LAB6: YOUR CODE
+     * (1) get a  proc_struct pointer p  with the minimum value of stride
+            (1.1) If using skew_heap, we can use le2proc get the p from rq->lab6_run_pol
+            (1.2) If using list, we have to search list to find the p with minimum stride value
+     * (2) update p;s stride value: p->lab6_stride
+     * (3) return p
+     */
+    skew_heap_entry_t *he = rq->lab6_run_pool;
+    if (he == NULL) return NULL;
+    struct proc_struct *p = le2proc(he, lab6_run_pool);
+    p->lab6_stride += BIG_STRIDE / p->lab6_priority;
+    return p;
 }
 
 /*
@@ -121,14 +138,20 @@ stride_pick_next(struct run_queue *rq) {
  */
 static void
 stride_proc_tick(struct run_queue *rq, struct proc_struct *proc) {
-     /* LAB6: YOUR CODE */
+    /* LAB6: YOUR CODE */
+    if (proc->time_slice > 0) {
+        proc->time_slice--;
+    }
+    if (proc->time_slice == 0) {
+        proc->need_resched = 1;
+    }
 }
 
 struct sched_class default_sched_class = {
-     .name = "stride_scheduler",
-     .init = stride_init,
-     .enqueue = stride_enqueue,
-     .dequeue = stride_dequeue,
-     .pick_next = stride_pick_next,
-     .proc_tick = stride_proc_tick,
+        .name = "stride_scheduler",
+        .init = stride_init,
+        .enqueue = stride_enqueue,
+        .dequeue = stride_dequeue,
+        .pick_next = stride_pick_next,
+        .proc_tick = stride_proc_tick,
 };
